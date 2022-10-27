1775e9c137027f1c2f69fe0d06041b6163fd2c54
Sebastian Carrion Ständer <sebastian.carrion@tum.de>
2022-10-27 13:34:21 +0200

    Example for testing added

---
+++ Code/AMCore/AMGUI/windows/AMFramework/AMFramework/N_GUI/Controller_NorasMainWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMFramework.N_GUI
+{
+    internal class Controller_NorasMainWindow : Controller.ControllerAbstract
+    {
+        // This is the constructor
+        public Controller_NorasMainWindow() 
+        { 
+        
+            // TODO: Add static page
+
+
+        }
+
+        /// <summary>
+        /// This is your page content used in Main window
+        /// </summary>
+        private object? _pageContent;
+        
+        /// <summary>
+        /// Public property with getter and setter
+        /// </summary>
+        public object? PageContent 
+        {
+            get { return _pageContent; }
+            set 
+            { 
+                _pageContent = value;
+
+                // If this is not set, when the content of the page changes
+                // the view does not update!
+
+                OnPropertyChanged(nameof(PageContent));
+            }
+        }
+
+        // TODO: Add commands
+
+
+
+    }
+}
---
+++ Code/AMCore/AMGUI/windows/AMFramework/AMFramework/N_GUI/Main_TestWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AMFramework.Nora_s_GUI
+{
+    /// <summary>
+    /// Interaction logic for Main_TestWindow.xaml
+    /// </summary>
+    public partial class Main_TestWindow : Window
+    {
+        public Main_TestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
